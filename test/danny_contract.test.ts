ed632cf030311362bc12ca8328837f364d694cce
Danny Tuppeny <danny@tuppeny.com>
2020-05-25 18:44:58 +0100

    Switch over to web3+geth

--- test/danny_contract.test.ts
+++ test/danny_contract.test.ts
@@ -1,13 +1,30 @@
-const DannyContract = artifacts.require("DannyContract");
+import * as assert from "assert";
+import * as fs from "fs";
+import Web3 from "web3";
+import { ContractsDannyContractSolDannyContract } from "../types/web3-v1-contracts/contracts_DannyContract_sol_DannyContract";
 
-contract("DannyContract", (accounts) => {
+describe("DannyContract", () => {
+  // geth --datadir geth-test-chain-dir --rpc --dev
   it("Can emit One", async () => {
-    const danny = await DannyContract.new();
-    const resp = await danny.one({ value: "3000000" });
+    const web3 = new Web3('http://127.0.0.1:8545/');
+    const accounts = await web3.eth.getAccounts();
+    console.log(`Account: ${accounts[0]}!`);
 
-    expect(resp.logs.length).equals(1);
-    expect(resp.logs[0].event).equals("One");
-    const args = resp.logs[0].args as any;
-    expect(args.foo).equals("One");
+    const contractAbi = fs.readFileSync("build/contracts/DannyContract.abi").toString();
+    const contractBin = fs.readFileSync("build/contracts/DannyContract.bin").toString();
+    const contract = new web3.eth.Contract(JSON.parse(contractAbi), undefined);
+    const c = await contract
+      .deploy({ data: `0x${contractBin}` })
+      .send({
+        from: accounts[0],
+      });
+
+    const d = c as any as ContractsDannyContractSolDannyContract;
+    let didEmitOne = false;
+    d.events.One((e) => didEmitOne = true);
+    await d.methods.one().call();
+    assert.ok(didEmitOne);
+
+    (web3.currentProvider as any).disconnect();
   });
 });
